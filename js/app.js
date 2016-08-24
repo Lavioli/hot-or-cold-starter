425486ce471b35378088b1149a0164cc411e7874
alexpoe22 <DRIPS@109s-mbp-3.pk5001z>
2016-08-24 13:18:57 -0700

    new

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
+var num;
+
+var generateNum = function () {
+	 num = Math.floor((Math.random()*100)+1);
+	 return num;
+ };
+
 
 $(document).ready(function(){
+
+	$("#guessButton").click(function(event){
+		// event.preventdefault();
+		var numInput = $("#userGuess").val();
+		});
+
+
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -12,6 +26,28 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
 });
 
+// function checkGuess(){
+// 	if(userGuess %1 !==0){
 
+
+
+
+
+//checks to make sure input is
+
+
+
+//a counter for guesses make a guess - group 1 lavie alex
+
+//what numbers were guessed in <lil> with ul #guesslist
+//
+
+
+//provide equation to let user know if they are close
+//feedback should be in h2#feedback
+//a counter for guesses "Make a Guess" - group 1 Lavie Alex
+//what numbers were guessed in <li> with ul#guesslist
+//make sure inputs for number is valid and prompt if it is not
